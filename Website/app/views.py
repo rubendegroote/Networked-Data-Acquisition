3f7af447b883dea32b73170a6aced6a5a8c1022b
woutergins <wouter.gins@gmail.com>
2015-03-13 14:31:08 +0100

    Cleaned up website
    Cleaned up the Website folder, adjusted DataServer.py to create a HTML
    in the correct folder.

--- Website/app/views.py
+++ Website/app/views.py
@@ -32,73 +32,67 @@ def before_request():
     g.user = current_user
 
 
-@app.route('/')
-@app.route('/index')
-@login_required
-def index():
-    user = g.user
-    posts = [  # fake array of posts
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template("index.html",
-                           title='Home',
-                           user=user,
-                           posts=posts)
+# @app.route('/')
+# @app.route('/index')
+# @login_required
+# def index():
+#     user = g.user
+#     posts = [  # fake array of posts
+#         {
+#             'author': {'nickname': 'John'},
+#             'body': 'Beautiful day in Portland!'
+#         },
+#         {
+#             'author': {'nickname': 'Susan'},
+#             'body': 'The Avengers movie was so cool!'
+#         }
+#     ]
+#     return render_template("index.html",
+#                            title='Home',
+#                            user=user,
+#                            posts=posts)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-@oid.loginhandler
-def login():
-    if g.user is not None and g.user.is_authenticated():
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        session['remember_me'] = form.remember_me.data
-        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
-    return render_template('login.html',
-                           title='Sign In',
-                           form=form,
-                           providers=app.config['OPENID_PROVIDERS'])
+# @app.route('/login', methods=['GET', 'POST'])
+# @oid.loginhandler
+# def login():
+#     if g.user is not None and g.user.is_authenticated():
+#         return redirect(url_for('index'))
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         session['remember_me'] = form.remember_me.data
+#         return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
+#     return render_template('login.html',
+#                            title='Sign In',
+#                            form=form,
+#                            providers=app.config['OPENID_PROVIDERS'])
 
-@oid.after_login
-def after_login(resp):
-    if resp.email is None or resp.email == "":
-        flash('Invalid login. Please try again.')
-        return redirect(url_for('login'))
-    user = User.query.filter_by(email=resp.email).first()
-    if user is None:
-        nickname = resp.nickname
-        if nickname is None or nickname == "":
-            nickname = resp.email.split('@')[0]
-        user = User(nickname=nickname, email=resp.email)
-        db.session.add(user)
-        db.session.commit()
-    remember_me = False
-    if 'remember_me' in session:
-        remember_me = session['remember_me']
-        session.pop('remember_me', None)
-    login_user(user, remember=remember_me)
-    return redirect(request.args.get('next') or url_for('index'))
+# @oid.after_login
+# def after_login(resp):
+#     if resp.email is None or resp.email == "":
+#         flash('Invalid login. Please try again.')
+#         return redirect(url_for('login'))
+#     user = User.query.filter_by(email=resp.email).first()
+#     if user is None:
+#         nickname = resp.nickname
+#         if nickname is None or nickname == "":
+#             nickname = resp.email.split('@')[0]
+#         user = User(nickname=nickname, email=resp.email)
+#         db.session.add(user)
+#         db.session.commit()
+#     remember_me = False
+#     if 'remember_me' in session:
+#         remember_me = session['remember_me']
+#         session.pop('remember_me', None)
+#     login_user(user, remember=remember_me)
+#     return redirect(request.args.get('next') or url_for('index'))
 
 
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/parameters', methods=['GET', 'POST'])
 def parameter():
     form = ParameterForm()
     if form.validate_on_submit():
-        # print(request.form['submit'])
-        # if 'submit' == request.form['submit']:
-        #     logging.info('Goes into submit')
-        #     setParams(form)
-        #     flash('Changed parameters!')
-        #     flashParams()
-        #     return redirect('/parameters')
         pass
     if request.method == 'POST':
         if 'stop' == request.form['submit']:
@@ -118,12 +112,6 @@ def parameter():
 
 
 def setParams(form):
-    # global parameters
-    # parameters = [form.parameter1.data,
-    #               form.parameter2.data,
-    #               form.parameter3.data,
-    #               form.parameter4.data,
-    #               form.saving.data]
     scanrange = np.linspace(form.parameter1.data,
                             form.parameter2.data,
                             form.parameter3.data)
