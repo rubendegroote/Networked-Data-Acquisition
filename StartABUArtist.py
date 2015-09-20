d71c1cd323f5f38d9b03dc6420465e0426f13f2c
Ruben de Groote <rdegroote@gmail.com>
2015-09-20 20:39:40 +0200

    Remove unneeded files

--- StartABUArtist.py
+++
@@ -1,9 +0,0 @@
-from Device import makeDevice
-import threading as th
-import asyncore
-
-def main():
-    m = makeDevice('ABU')
-
-if __name__ == '__main__':
-    main()
--- StartM2Artist.py
+++
@@ -1,9 +0,0 @@
-from backend.Device import makeDevice
-import threading as th
-import asyncore
-
-def main():
-    m = makeDevice('M2')
-
-if __name__ == '__main__':
-    main()
